5acc7811eb3e09ff1e94e5127d3cfbd7aae20d24
estebanCastaneira <estebanc_b@hotmail.com>
2025-01-06 10:18:31 -0300

    Minor update (erase comment)

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", 5432)  # Puerto por defecto 5432
+DB_PORT = os.getenv("DB_PORT", 5432)
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
